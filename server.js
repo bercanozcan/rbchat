6ca24d7a6a2502b67917d06471007cf48b411535
Bercan Özcan <beranozcan@gmail.com>
2020-03-16 22:41:22 +0300

    Server running

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const app = express();
+
+const http = require('http').Server(app);
+const PORT = 3000;
+
+app.listen(PORT, ()=>{
+    console.log("Listening port " + 3000);
+});

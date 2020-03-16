13580b02e872a339e533ea866c2beb68d1af5541
Bercan Özcan <beranozcan@gmail.com>
2020-03-16 22:50:53 +0300

    Socket install

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
-
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/index.html');
+});
 
 app.listen(PORT, ()=>{
     console.log("Listening port " + 3000);
+});
+
+io.on('connection', (socket)=>{
+    console.log("client is connected" + socket.id);
 });

5c5c9bd01849a206f32428c4083315e33b7c1472
Bercan Özcan <beranozcan@gmail.com>
2020-03-20 20:54:19 +0300

    Sound active

--- public/client.js
+++ public/client.js
@@ -1,18 +1,58 @@
+const recordAudio = () =>
+    new Promise(async resolve => {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        const mediaRecorder = new MediaRecorder(stream);
+        const audioChunks = [];
+
+        mediaRecorder.addEventListener("dataavailable", event => {
+            audioChunks.push(event.data);
+        });
+
+        const start = () => mediaRecorder.start();
+
+        const stop = () =>
+            new Promise(resolve => {
+                mediaRecorder.addEventListener("stop", () => {
+                    const audioBlob = new Blob(audioChunks);
+                    const audioUrl = URL.createObjectURL(audioBlob);
+                    const audio = new Audio(audioUrl);
+                    const play = () => audio.play();
+                    resolve({ audioBlob, audioUrl, play });
+                });
+
+                mediaRecorder.stop();
+            });
+
+        resolve({ start, stop });
+    });
+
+const sleep = time => new Promise(resolve => setTimeout(resolve, time));
+
+
 const socket = io();
 
 const message = document.getElementById('message'),
     output = document.getElementById('output'),
     button = document.getElementById('button');
 
-button.addEventListener('click', () => {
+
+const handleAction = async () => {
+    const recorder = await recordAudio();
+    const actionButton = document.getElementById('action');
+    actionButton.disabled = true;
+    actionButton.style.backgroundColor = "red";
+    recorder.start();
+    await sleep(3000);
+    const audio = await recorder.stop();
+    actionButton.style.backgroundColor = "#166cbf";
     socket.emit('createMessage', {
         handle: 'B',
-        message: message.value,
+        message: audio.audioUrl,
         createAt: new Date().getTime()
     });
     typing.innerHTML = '';
     message.value = '';
-});
+}
 
 message.addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
@@ -25,11 +65,11 @@ socket.on('createMessage', (data) => {
     const formattedTime = moment(data.createAt).format('LT');
     console.log(data);
     var html = '';
-    html += '<div class="message">';
+    html += '<div class="message me">';
     html += '<div class="bubble">';
-    html += data.message;
+    html += '<audio controls src="'+data.message+'"></audio>';
     html += '</div>';
-    html += '<div class="time">'+formattedTime+'</div>';
+    html += '<div class="time">' + formattedTime + '</div>';
     html += '</div>';
 
     output.innerHTML += html;
@@ -44,3 +84,6 @@ socket.on('connect', () => {
 socket.on('disconnect', () => {
     console.log('Disconnected to server');
 });
+
+
+
